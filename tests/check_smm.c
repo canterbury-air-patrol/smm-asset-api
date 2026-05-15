1ae97b71297e910febc34e7fd57875462304e78b
Scott Parlane <scott@parlanenz.com>
2026-05-15 23:01:42 +1200

    security: refactor and harden CSRF token extraction
    - Move extraction logic to testable smm_parse_csrf_token function.
    - Add length validation (< 256 bytes) and character validation ([A-Za-z0-9_-]).
    - Add SMM_CONNECTION_PROTOCOL_ERROR state for missing tokens.
    - Add unit tests for CSRF extraction edge cases.
    
    Fixes todo/03-csrf-token-extraction-hardening.md

--- tests/check_smm.c
+++ tests/check_smm.c
@@ -1,25 +1,60 @@
 #include <stdlib.h>
 #include <check.h>
 #include "smm-asset.h"
+#include "smm-asset-internal.h"
 
-START_TEST(test_placeholder)
+START_TEST(test_csrf_extraction)
 {
-    ck_assert_int_eq(1, 1);
+    const char *html = "<html><body><input type=\"hidden\" name=\"csrfmiddlewaretoken\" value=\"abcd1234\"></body></html>";
+    char *token = smm_parse_csrf_token(html, strlen(html));
+    ck_assert_ptr_nonnull(token);
+    ck_assert_str_eq(token, "abcd1234");
+    free(token);
+}
+END_TEST
+
+START_TEST(test_csrf_extraction_missing)
+{
+    const char *html = "<html><body><input type=\"hidden\" name=\"somethingelse\" value=\"abcd1234\"></body></html>";
+    char *token = smm_parse_csrf_token(html, strlen(html));
+    ck_assert_ptr_null(token);
+}
+END_TEST
+
+START_TEST(test_csrf_extraction_invalid_chars)
+{
+    const char *html = "<html><body><input type=\"hidden\" name=\"csrfmiddlewaretoken\" value=\"abcd!@#$\"></body></html>";
+    char *token = smm_parse_csrf_token(html, strlen(html));
+    ck_assert_ptr_null(token);
+}
+END_TEST
+
+START_TEST(test_csrf_extraction_too_long)
+{
+    char html[1024];
+    char value[300];
+    memset(value, 'a', 299);
+    value[299] = '\0';
+    snprintf(html, sizeof(html), "<html><body><input type=\"hidden\" name=\"csrfmiddlewaretoken\" value=\"%s\"></body></html>", value);
+    
+    char *token = smm_parse_csrf_token(html, strlen(html));
+    ck_assert_ptr_null(token);
 }
 END_TEST
 
 Suite * smm_suite(void)
 {
     Suite *s;
-    TCase *tc_core;
+    TCase *tc_csrf;
 
     s = suite_create("SMM");
 
-    /* Core test case */
-    tc_core = tcase_create("Core");
-
-    tcase_add_test(tc_core, test_placeholder);
-    suite_add_tcase(s, tc_core);
+    tc_csrf = tcase_create("CSRF");
+    tcase_add_test(tc_csrf, test_csrf_extraction);
+    tcase_add_test(tc_csrf, test_csrf_extraction_missing);
+    tcase_add_test(tc_csrf, test_csrf_extraction_invalid_chars);
+    tcase_add_test(tc_csrf, test_csrf_extraction_too_long);
+    suite_add_tcase(s, tc_csrf);
 
     return s;
 }
