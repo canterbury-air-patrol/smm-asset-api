b61aee7452399828831bdd3ab8417ecad48ba3f4
Scott Parlane <scott@parlanenz.com>
2026-05-15 23:03:46 +1200

    tests: refactor asset parsing into testable function and add unit tests
    - Move asset parsing logic to smm_parse_assets.
    - Fix unsafe realloc pattern (addresses part of issue #06).
    - Fix memory leak in smm_asset_get_assets on error paths (addresses part of issue #04).
    - Remove stray printfs (addresses part of issue #13).
    - Add unit tests for successful and failing asset parsing scenarios.

--- src/smm-asset-internal.h
+++ src/smm-asset-internal.h
@@ -95,6 +95,7 @@ struct smm_curl_res_s *smm_connection_curl_retrieve_url (smm_connection conn, co
 							 size_t (*write_func) (char *ptr, size_t size, size_t nmemb, void *userdata), void *write_data, bool json);
 bool smm_asset_connection_login (smm_connection connection);
 char *smm_parse_csrf_token (const char *data, size_t len);
+bool smm_parse_assets (smm_connection connection, const char *data, size_t len, smm_assets * assets, size_t * assets_count);
 
 smm_asset smm_asset_create (smm_connection connection, const char *name, const char *type, long long asset_id, long long asset_type_id);
 void smm_asset_free_asset (smm_asset assets);
